b2500c458d8795da26e62905a2b21da28525b5e0
Davide Fossacecchi <davide.fossacecchi@accuratesolutions.it>
2015-11-17 16:53:48 +0100

    Put the login script in .ready()
    - Get the scripts out of the body tag

--- js/login.js
+++ js/login.js
@@ -1,99 +1,101 @@
-var method="POST";
-$('button').click(function(event){login(event)});
+$(document).ready(function(){
+  var method="POST";
+  $('button').click(function(event){login(event)});
 
-//eye candy for the register function
-$('#register').click(function(event){
-  event.preventDefault();
-  method="PUT";
-  $('input[name="password"]').slideDown(function(){
-    $('input[name="password_conf"]').slideDown();
-    $('#register').slideUp(function(){
-      $('#login').slideDown();
-      $('#recover').slideDown(function(){
-        $('button[type="submit"]').html("Sign Up");
+  //eye candy for the register function
+  $('#register').click(function(event){
+    event.preventDefault();
+    method="PUT";
+    $('input[name="password"]').slideDown(function(){
+      $('input[name="password_conf"]').slideDown();
+      $('#register').slideUp(function(){
+        $('#login').slideDown();
+        $('#recover').slideDown(function(){
+          $('button[type="submit"]').html("Sign Up");
+        });
       });
     });
   });
-});
 
-//eye candy for the login function
-$('#login').click(function(event){
-  event.preventDefault();
-  method="POST";
-  $('input[name="password"]').slideDown(function(){
-    $('input[name="password_conf"]').slideUp();
-    $('#login').slideUp(function(){
-      $('#register').slideDown();
-      $('#recover').slideDown(function(){
-        $('button[type="submit"]').html("Login");
+  //eye candy for the login function
+  $('#login').click(function(event){
+    event.preventDefault();
+    method="POST";
+    $('input[name="password"]').slideDown(function(){
+      $('input[name="password_conf"]').slideUp();
+      $('#login').slideUp(function(){
+        $('#register').slideDown();
+        $('#recover').slideDown(function(){
+          $('button[type="submit"]').html("Login");
+        });
       });
     });
   });
-});
 
-//eye candy for the recover function
-$('#recover').click(function(event){
-  event.preventDefault();
-  method="GET";
-  $('input[name="password_conf"]').slideUp(function(){
-    $('input[name="password"]').slideUp();
-    $('#register').slideDown(function(){
-      $('#login').slideDown();
-      $('#recover').slideUp(function(){
-        $('button[type="submit"]').html("Recover");
+  //eye candy for the recover function
+  $('#recover').click(function(event){
+    event.preventDefault();
+    method="GET";
+    $('input[name="password_conf"]').slideUp(function(){
+      $('input[name="password"]').slideUp();
+      $('#register').slideDown(function(){
+        $('#login').slideDown();
+        $('#recover').slideUp(function(){
+          $('button[type="submit"]').html("Recover");
+        });
       });
     });
   });
-});
 
-function login(event){
-  event.preventDefault();
-  if((method == "PUT") && ($("input[name='password']").val()!=$("input[name='password_conf']").val())){
-    $("#message").slideUp(function(){
-      
-      $('#message').removeClass("success").addClass("error");
-      $("#message").html("The two password fields don't match");
-      $("#message").slideDown();
-
-    });
-  }
-  else{
-    var form = $('form').serialize();
-    var jqxhr = $.ajax({
-        type: method,
-        url:'login.php',
-        data: form
-    }).success(function(resp){
+  function login(event){
+    event.preventDefault();
+    if((method == "PUT") && ($("input[name='password']").val()!=$("input[name='password_conf']").val())){
       $("#message").slideUp(function(){
-        if(method == "GET"){
-          $('#message').removeClass("error").addClass("success");
-          $("#message").html("We have sent an email to you");
-          $("#message").slideDown();
-        }
-        else{
-          location.href="manage.php";
-        }
-      });//location.href=resp;
-      
-    }).fail(function(data){
-      $("#message").slideUp(function(){
+        
         $('#message').removeClass("success").addClass("error");
-        switch(jqxhr.status){
-          case 404:
-            $("#message").html("User not registered");
-          break;
-          case 400:
-            $("#message").html("There is some empty field");
-          break;
-          case 401:
-            $("#message").html("Incorrect username and/or password");
-          break;
-          case 409:
-            $("#message").html("That user is already registered");
-          break;
-        }
+        $("#message").html("The two password fields don't match");
         $("#message").slideDown();
+
       });
-    });
+    }
+    else{
+      var form = $('form').serialize();
+      var jqxhr = $.ajax({
+          type: method,
+          url:'login.php',
+          data: form
+      }).success(function(resp){
+        $("#message").slideUp(function(){
+          if(method == "GET"){
+            $('#message').removeClass("error").addClass("success");
+            $("#message").html("We have sent an email to you");
+            $("#message").slideDown();
+          }
+          else{
+            location.href="manage.php";
+          }
+        });//location.href=resp;
+        
+      }).fail(function(data){
+        $("#message").slideUp(function(){
+          $('#message').removeClass("success").addClass("error");
+          switch(jqxhr.status){
+            case 404:
+              $("#message").html("User not registered");
+            break;
+            case 400:
+              $("#message").html("There is some empty field");
+            break;
+            case 401:
+              $("#message").html("Incorrect username and/or password");
+            break;
+            case 409:
+              $("#message").html("That user is already registered");
+            break;
+          }
+          $("#message").slideDown();
+        });
+      });
+    }
   }
-}
+});
